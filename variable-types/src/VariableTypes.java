98ca296aa7a85510f263f084756a116bc21e0fc9
Gabriel A. Sato <gwork1324@gmail.com>
2024-08-08 20:27:23 -0300

    content of variable types

--- variable-types/src/VariableTypes.java
+++ variable-types/src/VariableTypes.java
@@ -1,9 +1,28 @@
 public class VariableTypes {
     public static void main(String[] args) throws Exception {
+
+        // study reminder
+        // primitive types
+        // String class as text representation in the application
+
+        String myName = "Gabs";
+
         double minimumSalary = 2500;
 
         short shortNumber = 1;
+
         int normalNumber = shortNumber;
+
         short shortNumber2 = (short) normalNumber;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        // variable name in uppercase to determine that the value does not change
+        final double VALUE_PI = 3.14;
+
     }
 }
