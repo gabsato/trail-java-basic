e2ee115edf789153b7430b73e138784746289465
Gabriel A. Sato <gwork1324@gmail.com>
2024-08-13 21:59:17 -0300

    putting "println"

--- variable-types/src/VariableTypes.java
+++ variable-types/src/VariableTypes.java
@@ -15,14 +15,30 @@ public class VariableTypes {
 
         short shortNumber2 = (short) normalNumber;
 
+        System.out.println("short number: " + shortNumber2);
+
+        System.out.println("==========================================================");
+
         int number = 5;
+
+        System.out.println("Example int number: " + number);
+
+        System.out.println("==========================================================");
 
         number = 10;
 
-        System.out.println(number);
+        System.out.println("Name: " + myName + "\nMinimun salary: " + minimumSalary);
+
+        System.out.println("==========================================================");
+
+        System.out.println("After changing the variable int number: " + number);
+
+        System.out.println("==========================================================");
 
         // variable name in uppercase to determine that the value does not change
         final double VALUE_PI = 3.14;
+
+        System.out.println("final double variable, value PI: " + VALUE_PI);
 
     }
 }
