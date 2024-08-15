a83004c232635358cc13e02b0fe8c2830f165a20
Gabriel A. Sato <gwork1324@gmail.com>
2024-08-15 18:28:43 -0300

    condition

--- variable-types/src/Operators.java
+++ variable-types/src/Operators.java
@@ -2,28 +2,41 @@ public class Operators {
 
     public static void main(String[] args) {
 
-        String nameOne = "Mahou";
-        String nameTwo = new String("Mahou");
+        // String nameOne = "Mahou";
+        // String nameTwo = new String("Mahou");
 
-        System.out.println(nameOne.equals(nameTwo));
+        // System.out.println(nameOne.equals(nameTwo));
 
-        int number1 = 1;
-        int number2 = 2;
+        // int number1 = 1;
+        // int number2 = 2;
 
-        boolean yesNo = number1 == number2;
+        // boolean yesNo = number1 == number2;
 
-        if (number1 == number2) {
-            System.out.println("The condition is true");
+        // if (number1 == number2) {
+        // System.out.println("The condition is true");
+        // }
+
+        // System.out.println("The number1 equals number2 " + yesNo);
+
+        // yesNo = number1 != number2;
+
+        // System.out.println("The number1 different number2 " + yesNo);
+
+        // yesNo = number1 > number2;
+
+        // System.out.println("The number1 is greater than number2 " + yesNo);
+
+        // CONDITION
+        boolean condition1 = true;
+        boolean condition2 = true;
+
+        if (condition1 && (7 > 4)) {
+            System.out.println("Both conditions are true");
         }
 
-        System.out.println("The number1 equals number2 " + yesNo);
-
-        yesNo = number1 != number2;
-
-        System.out.println("The number1 different number2 " + yesNo);
-
-        yesNo = number1 > number2;
-
-        System.out.println("The number1 is greater than number2 " + yesNo);
+        if (condition1 || condition2) {
+            System.out.println("One of the condition is true");
+        }
+        System.out.println("End");
     }
 }
