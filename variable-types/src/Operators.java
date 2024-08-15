87a1e706dca0e0f1122dac6e326c5967001be0a1
Gabriel A. Sato <gwork1324@gmail.com>
2024-08-15 18:19:26 -0300

    comparator

--- variable-types/src/Operators.java
+++ variable-types/src/Operators.java
@@ -1,14 +1,29 @@
 public class Operators {
 
     public static void main(String[] args) {
-        int a, b;
 
-        a = 6;
-        b = 6;
+        String nameOne = "Mahou";
+        String nameTwo = new String("Mahou");
 
-        String result = a == b ? "true" : "false";
+        System.out.println(nameOne.equals(nameTwo));
 
-        System.out.println(result);
+        int number1 = 1;
+        int number2 = 2;
 
+        boolean yesNo = number1 == number2;
+
+        if (number1 == number2) {
+            System.out.println("The condition is true");
+        }
+
+        System.out.println("The number1 equals number2 " + yesNo);
+
+        yesNo = number1 != number2;
+
+        System.out.println("The number1 different number2 " + yesNo);
+
+        yesNo = number1 > number2;
+
+        System.out.println("The number1 is greater than number2 " + yesNo);
     }
 }
